1952c9c07efb296d677fc98e49ee941fb67224bc
MegaTurtl <64576737+MegaTurtl@users.noreply.github.com>
2026-03-05 02:07:52 +1100

    light mode less sucky now
    added some images to make the wip pages feel less empty, and made the light mode bg less... light

--- src/_data/statuses.js
+++ src/_data/statuses.js
@@ -1,5 +1,11 @@
 module.exports = [
     {
+        date: "2026-03-05",
+        emoji: "😔",
+        emojiLabel: "they hate to see me winning",
+        text: "modrinth does NOT wanna see me win. 7 days since I uploaded Chiselmon (my first public mod) and it's still not approved </3"
+    },
+    {
         date: "2026-02-27",
         emoji: "😭",
         emojiLabel: "where has the timeeee gone",
